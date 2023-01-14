837d4405fb583ce99e3f383e8c470583784f6140
Soumiya Ayouch <ayouch.somaya@gmail.com>
2023-01-14 13:47:35 +0100

    first commit

---
+++ resources/js/app.jsx
@@ -0,0 +1,9 @@
+import './bootstrap';
+import '../css/app.css'
+
+import ReactDOM from 'react-dom/client';        
+import Home from './pages/Home';
+
+ReactDOM.createRoot(document.getElementById('app')).render(     
+    <Home />        
+);
---
+++ resources/js/pages/Home.jsx
@@ -0,0 +1,4 @@
+export default function Home() {
+    const heading = "Laravel 9 with React JS";
+    return <div> {heading}</div>;
+}
