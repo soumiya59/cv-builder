e6710597a92a1c49f3c9731fb29809a17b2df7a7
Soumiya Ayouch <ayouch.somaya@gmail.com>
2023-01-14 19:23:34 +0100

    added tailwindCSS and bootstrap5.3

--- resources/js/pages/Home.jsx
+++ resources/js/pages/Home.jsx
@@ -1,4 +1,4 @@
 export default function Home() {
     const heading = "Laravel 9 with React JS";
-    return <div> {heading}</div>;
+    return <div className="text-3xl font-bold text-purple-600"> {heading}</div>;
 }
---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./resources/**/*.blade.php",
+    "./resources/**/*.jsx",
+    "./resources/**/*.js",
+    "./resources/**/**/*.jsx",
+    "./resources/**/**/*.js",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
