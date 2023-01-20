632cbf693cb1e0ca53be9261bd5d9c8f949be103
Soumiya Ayouch <ayouch.somaya@gmail.com>
2023-01-20 17:28:40 +0100

    deleted unnecesary files

--- resources/js/app.jsx
+++ resources/js/app.jsx
@@ -3,7 +3,6 @@ import '../css/app.css'
 import GlobalStyles from '../css/globalstyles';
 
 import ReactDOM from 'react-dom/client';        
-import Home from './pages/Home';
 import Header from './pages/Header';
 import Footer from './pages/Footer'
 ReactDOM.createRoot(document.getElementById('app')).render(    
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,7 +3,7 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title>Laravel 9 vite with react</title>
+        <title></title>
 
         @viteReactRefresh
         @vite('resources/js/app.jsx')
