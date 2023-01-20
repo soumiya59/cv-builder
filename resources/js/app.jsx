20d8cd74180c787514b7a19ec54a7c2c69fba97a
Soumiya Ayouch <ayouch.somaya@gmail.com>
2023-01-20 16:42:05 +0100

    header is done

--- resources/js/app.jsx
+++ resources/js/app.jsx
@@ -1,9 +1,11 @@
 import './bootstrap';
 import '../css/app.css'
+import GlobalStyles from '../css/globalstyles';
 
 import ReactDOM from 'react-dom/client';        
 import Home from './pages/Home';
+import Header from './pages/Header';
 
 ReactDOM.createRoot(document.getElementById('app')).render(     
-    <Home />        
+    <Header />        
 );
