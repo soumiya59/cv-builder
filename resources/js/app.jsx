54e2a6dc7fa1853d26679b3ec85380256c0b99c0
Kouriri Zohayr <kouririzouhair88@gmail.com>
2023-01-23 12:21:02 +0100

    add page profile

--- resources/js/app.jsx
+++ resources/js/app.jsx
@@ -1,13 +1,12 @@
 import './bootstrap';
 import '../css/app.css'
-import GlobalStyles from '../css/globalstyles';
-
+import '../css/globaleCss.css'
 import ReactDOM from 'react-dom/client';        
 import Header from './pages/Header';
 import Footer from './pages/Footer'
 ReactDOM.createRoot(document.getElementById('app')).render(    
     <>
-        <Header />        
-        <Footer />        
+         <Header />    
+         <Footer />       
     </> 
 );
