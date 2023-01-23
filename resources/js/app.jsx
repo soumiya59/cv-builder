766f3260f5a67cf289279bf70b109cefcef85a10
Soumiya Ayouch <ayouch.somaya@gmail.com>
2023-01-23 13:23:47 +0100

    updated homepage

--- resources/js/app.jsx
+++ resources/js/app.jsx
@@ -1,21 +1,9 @@
 import './bootstrap';
 import '../css/app.css'
 import GlobalStyles from '../css/globalstyles';
-
 import ReactDOM from 'react-dom/client';        
-import Header from './pages/Header';
-import Footer from './pages/Footer'
-import Modeles from './pages/Modeles';
-import Carousel from './pages/Carousel';
-import Telecharger from './pages/Telecharger'
-import Home from './pages/Home';
 
 ReactDOM.createRoot(document.getElementById('app')).render(    
     <>
-        {/* <Header />         */}
-        {/* <Modeles />         */}
-        {/* <Footer />         */}
-        <Home /> 
-        {/* <Telecharger /> */}
     </> 
 );
