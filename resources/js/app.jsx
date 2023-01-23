fc22fae1529524bd1cd9e7b8af7ce3f46d90f3e7
Kouriri Zohayr <kouririzouhair88@gmail.com>
2023-01-23 13:30:12 +0100

    profile

--- resources/js/app.jsx
+++ resources/js/app.jsx
@@ -1,17 +1,18 @@
 import './bootstrap';
 import '../css/app.css'
-import '../css/globaleCss.css'
+import Profile from './pages/Profile'
 import ReactDOM from 'react-dom/client';        
 import Header from './pages/Header';
 import Footer from './pages/Footer'
-import Modeles from './pages/Modeles';
+/* import Modeles from './pages/Modeles';
 import Carousel from './pages/Carousel';
 import Telecharger from './pages/Telecharger'
-import Home from './pages/Home';
+import Home from './pages/Home'; */
 
 ReactDOM.createRoot(document.getElementById('app')).render(    
     <>
-        <Header />        
+        <Header />   
+             <Profile/> 
         <Footer />        
     </> 
 );
