7b898915338306b136d2f41a0ea1955a13ee8c41
Kouriri Zohayr <kouririzouhair88@gmail.com>
2023-01-23 12:53:22 +0100

    page profile

--- resources/js/pages/Component_Zohayr/Profile.jsx
+++ resources/js/pages/Component_Zohayr/Profile.jsx
@@ -6,7 +6,7 @@ const Profile = () => {
                 edition :'Dérnier édition : 16/01/2023 ',  
             }]
     return (
-        <div className="container mx-auto  bg-white mb-10 pb-10" >
+        <div className="container mx-auto    bg-white mb-10 pb-10" >
             <h1 className=' text-3xl font-Montserrat font-bold  leading-none tracking-tight pt-10 pb-20  text-gray-900 text-center md:text-4xl sm:text-4xl lg:text-5xl'>Mes CV</h1>
             <div className='  grid md:grid-cols-1 lg:grid-cols-2 w-5/6 mx-auto sm:grid-cols-1  gap-x-10 gap-y-14    '>
             {Cvs.map(cv=>
@@ -25,6 +25,7 @@ const Profile = () => {
                             </div>
                            
                             <div className='flex justify-start ml-2'>
+
                                 <img className='w-6 h-6 md:w-10 md:h-auto ms:mt-1 mt-1  lg:-mt-1 lg:-ml-1   -mr-3 mb-2' src="images/icons8-trash-50.png" alt="" /><a className=' mb-2 ml-4 md:text-2xl sm:text-sm font-Montserrat text-gray-400/100' >Supprimer</a>
                             </div>
                         </div>
@@ -32,10 +33,10 @@ const Profile = () => {
                 )} 
             </div> 
             <div class="grid place-items-center mt-20">
-            <button id="btn-creastion" className="pb-7    text-center   h-12  w-72 text-white rounded-3xl bg-darkpink hover:opacity-90">
-                    <p className="font-Montserrat   text-3xl">créer nouveau cv</p>
-            </button>
-</div>
+                    <button id="btn-creastion" className="pb-7    text-center   h-12  w-72 text-white rounded-3xl bg-darkpink hover:opacity-90">
+                            <p className="font-Montserrat   text-3xl">créer nouveau cv</p>
+                    </button>
+            </div>
             
         </div>
        
