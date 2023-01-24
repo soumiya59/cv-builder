345f10f49526b5c3abd756160540a9661f2b5416
soumiya59 <soumiyaus@gmail.com>
2023-01-24 20:14:42 +0100

    cleaned app.jsx

--- resources/js/app.jsx
+++ resources/js/app.jsx
@@ -1,38 +1,16 @@
 import './bootstrap';
 import '../css/app.css'
-<<<<<<< HEAD
-
 import ReactDOM from 'react-dom/client';        
-import Header from './pages/Header';
-import Footer from './pages/Footer'
-=======
 import GlobalStyles from '../css/globalstyles';
-import ReactDOM from 'react-dom/client';      
 import { BrowserRouter as Router , Routes, Route} from "react-router-dom";  
->>>>>>> 79708b31749972e8fbb00e6b6efdcc0632e5d022
 import Modeles from './pages/Modeles';
 import Telecharger from './pages/Telecharger'
 import Profile from '../js/pages/Profile'
 import Home from './pages/Home';
-
-import Profile from './pages/Profile';
 import TelechargerModal from './pages/TelechargerModal';
 
 
 ReactDOM.createRoot(document.getElementById('app')).render(    
-<<<<<<< HEAD
-    
-    <>
-        {/* <Header />         */}
-        {/* <Modeles />         */}
-        {/* <Footer />         */}
-        {/*  <Home />   */}
-         <Profile /> 
-         {/* <Modal /> */}
-       {/*  <TelechargerModal /> */}
-        {/* <Telecharger /> */}
-    </> 
-=======
     <Router>
         {/* <Home />  */}
         <Routes>
@@ -42,5 +20,4 @@ ReactDOM.createRoot(document.getElementById('app')).render(
               <Route path="/telecharger" element={<Telecharger  />} />
         </Routes>
     </Router> 
->>>>>>> 79708b31749972e8fbb00e6b6efdcc0632e5d022
 );
