7603d8f9ecd6126b4c0b7163d3dcf3cd9f5a6541
soumiya59 <soumiyaus@gmail.com>
2023-05-31 09:42:01 +0100

    fixed syntax errors

--- database/factories/CvFactory.php
+++ database/factories/CvFactory.php
@@ -8,7 +8,7 @@ use Illuminate\Database\Eloquent\Factories\Factory;
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Cv>
  */
-class cvFactory extends Factory
+class CvFactory extends Factory
 {
     /**
      * Define the model's default state.
